187f969b77362c728256b59b2a933ec11d9ec108
hielv <elvira-alieva-03@list.ru>
2024-04-03 13:20:00 +0300

    КАКОЙ-ТО КОММИТ

--- server.py
+++ server.py
@@ -1,1 +1,12 @@
-print('hello')
+import socket
+sock = socket.socket()
+
+sock.bind(('', 9090))
+sock.listen(1)
+conn, addr = sock.accept()
+while True:
+     data = conn.recv(1024)
+     if not data:
+         break
+     conn.send(data.upper())
+conn.close()
